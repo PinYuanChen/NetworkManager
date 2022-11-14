b27f3ce5a259f25631391c8480f3a0121eaa0c0c
Patrick <champion790713@gmail.com>
2022-11-14 13:31:21 +0800

    create and setup endpoint and network manager files

---
+++ NetworkManager/Endpoint.swift
@@ -0,0 +1,30 @@
+//
+// Created on 2022/11/14.
+//
+
+import Foundation
+
+protocol Endpoint {
+    /// HTTP or HTTPS
+    var scheme: String {
+        get
+    }
+    
+    var baseURL: String {
+        get
+    }
+    
+    var path: String {
+        get
+    }
+    
+    /// [URLQueryItem(name: "api_key", value: API_KEY)]
+    var parameters: [URLQueryItem] {
+        get
+    }
+    
+    /// "GET" or "POST"
+    var method: String {
+        get
+    }
+}
---
+++ NetworkManager/ViewController.swift
@@ -0,0 +1,16 @@
+//
+// Created on 2022/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
